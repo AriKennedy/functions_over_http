8040f3b89e85b509cbbf74df0f01ccd9b75e60b8
Arianna Kennedy <akennedy25@basecampcodingacademy.org>
2024-10-17 09:23:43 -0500

    Added Functions

---
+++ app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.http.request import HttpRequest
+
+# Create your views here.
+def hey_you(request, name):
+    return HttpResponse(f"Hello {name}!")
+
+def how_old(request, end, birthyear):
+    age = int(end) - int(birthyear)
+    return HttpResponse(age)
+
+def canITakeYourOrder(request, burgers, fries, drinks):
+    total = (int(burgers) * 4.50) + (int(fries) * 1.50) + (int(drinks) * 1.00)
+    return HttpResponse(f"Your total is ${total}")
